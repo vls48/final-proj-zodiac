87a6dbe90cf39afb94294ceeb26507ac8d9daa13
vls48 <vls48@drexel.edu>
2017-02-26 17:59:32 -0500

    week7
    added help menu, click functionality complete

--- whatplantjava.js
+++ whatplantjava.js
@@ -1,15 +1,30 @@
 //Global variables
 var firstName;
 var birthDayOBJ;
-
-
-function calcPlant() {
-	var outputArea = document.getElementById('displayImg');
+var outputArea = document.getElementById('displayImg');
 	//outputarea variable replaces displayMsg
-
-	firstName = document.getElementById('uname').value;
+firstName = document.getElementById('uname').value;
 	//returns first name entered
 
+
+var descCarnation = '<p> December 22 - January 19 </p> You are the carnation. You are partical and disciplined, but also somewhat pessimistic.';
+var descDaffodil = '<p> November 22 - December 21 </p> You are the daffodil. You are an optimistic individual. You are bright and promising, like the first sign of spring.';
+var descVenus = '<p> October 23 - November 21 </p> You are the venus fly trap. You are powerful, exciting. Though you are a bold individual, you can also be secretive.';
+var descDaisy = '<p> September 23 - October 22 </p> You are the daisy. You are charming, flirtatious and easygoing.';
+var descAloe = '<p> August 23 - September 22 </p> You are the aloe plant. You are reliable and practical. You are noted for you ability to help others.';
+var descIvy = '<p> July 23 - August 22 </p> You are ivy. You are a creative, expansive, and generous person. You like to intertwine your way into situations and make your opinion heard.';
+var descBleeding = '<p> June 21 - July 22 </p> You are the bleeding heart. You are an emotional individual and are very family-oriented. You are loyal and sympathetic.';
+var descSun =  '<p> May 21 - June 20 </p> You are the sunflower. You are youthful lovely, and popular. Your bright demenor is like the sun.';
+var descBonsai = '<p> April 20 - May 20 </p> You are a bonsai tree. You are reliable and persistent. You are a inflexible and stable person.';
+var descCactus = '<p> March 21 - April 19 </p> You are the cactus. You are a daredevil. You are also a confident and dynamic individual.';
+var descTulip = '<p> February 19 - March 20 </p> You are the tulip. You are kind and sensitive, and also very selfless.';
+var descHanging = '<p> January 20 - February 18 </p> You are the hanging plant. You are friendly and original, and also independent and unpredictable.';
+
+//
+//
+//user submits information and output function here
+function calcPlant() {
+	
 	birthDayOBJ = new
 	Date(document.getElementById('bday').value);
 	
@@ -31,32 +46,32 @@ function calcPlant() {
 	var astroSign;
 
 	if ((monthNum == 12 && dayNum >= 22) || (monthNum == 1 && dayNum <= 19)){
-		astroSign = 'December 22 - January 19 <br> You are the carnation. You are partical and disciplined, but also somewhat pessimistic';
+		astroSign = descCarnation;
 	} else if ((monthNum == 11 && dayNum >= 22) || (monthNum == 12 && dayNum <=21)){
-		astroSign = 'November 22 - December 21 <br> You are the daffodil. You are an optimistic individual. You are bright and promising, like the first sign of spring';
+		astroSign = descDaffodil;
 	} else if ((monthNum == 10 && dayNum >= 24) || (monthNum == 11 && dayNum <=21)){
-		astroSign = 'October 23 - November 21 <br> You are the venus fly trap. You are powerful, exciting. Though you are a bold individual, you can also be secretive';
+		astroSign = descVenus;
 	} else if ((monthNum == 9 && dayNum >= 23) || (monthNum == 10 && dayNum <=23)){
-		astroSign = 'September 23 - October 22 <br> You are the daisy. You are charming, flirtatious and easygoing';
+		astroSign = descDaisy;
 	} else if ((monthNum == 8 && dayNum >= 23) || (monthNum == 9 && dayNum <=22)){
-		astroSign = 'August 23 - September 22 <br> You are the aloe plant. You are reliable and practical. You are noted for you ability to help others';
+		astroSign = descAloe;
 	} else if ((monthNum == 7 && dayNum >= 23) || (monthNum == 8 && dayNum <=22)){
-		astroSign = 'July 23 - August 22 <br> You are ivy. You are a creative, expansive, and generous person. You like to intertwine your way into situations and make your opinion heard';
+		astroSign = descIvy;
 	} else if ((monthNum == 6 && dayNum >= 22) || (monthNum == 7 && dayNum <=22)){
-		astroSign = 'June 21 - July 22 <br> You are the bleeding heart. You are an emotional individual and are very family-oriented. You are loyal and sympathetic';
+		astroSign = descBleeding;
 	} else if ((monthNum == 5 && dayNum >= 21) || (monthNum == 6 && dayNum <=21)){
-		astroSign = 'May 21 - June 20 <br> You are the sunflower. You are youthful lovely, and popular. Your bright demenor is like the sun';
+		astroSign = descSun;
 	} else if ((monthNum == 4 && dayNum >= 20) || (monthNum == 5 && dayNum <=20)){
-		astroSign = 'April 20 - May 20 <br> You are a bonsai tree. You are reliable and persistent. You are a inflexible and stable person';
+		astroSign = descBonsai;
 	} else if ((monthNum == 3 && dayNum >= 21) || (monthNum == 4 && dayNum <=19)){
-		astroSign = 'March 21 - April 19 <br> You are the cactus. You are a daredevil and a cofident and dynamic individual';
+		astroSign = descCactus;
 	} else if ((monthNum == 2 && dayNum >= 19) || (monthNum == 3 && dayNum <=20)){
-		astroSign = 'February 19 - March 20 <br> You are the tulip. You are kind and sensitive, and also very selfless';
+		astroSign = descTulip;
 	} else {
-		astroSign = 'January 20 - February 18 <br> You are the hanging plant. You are friendly and original, and also independent and unpredictable';
+		astroSign = descHanging;
 	}
 
-	outputArea.innerHTML = "Hello " + firstName + ". <br> " + astroSign + ".";
+	outputArea.innerHTML = "<p>Hello " + firstName + ". <p> " + astroSign;
 
 return false;
 }
@@ -64,11 +79,15 @@ return false;
 
 //
 //
-// 
+// check function
 function highlight() {
   console.log('highlight called');
 }
 
+
+//
+//
+// listeners and parameter passing
 var aloeObj = document.getElementById('aloe');
 var bleedingObj = document.getElementById('bleeding');
 var bonsaiObj = document.getElementById('bonsai');
@@ -82,11 +101,15 @@ var sunObj = document.getElementById('sun');
 var tulipObj = document.getElementById('tulip');
 var trapObj = document.getElementById('trap');
 
+var helpObj = document.getElementById('help');
+var helpMenuObj = document.getElementById('help-menu')
+
 
 //var displayObj = document.getElementById('display');
 var allPlantsObj = document.getElementById('images');
-
-
+//
+//
+//event listeners plants
 aloeObj.addEventListener('click', function(evt) {
 	displayZodSign(evt, 'aloe');
 }, false);
@@ -134,7 +157,31 @@ tulipObj.addEventListener('click', function(evt) {
 trapObj.addEventListener('click', function(evt) {
 	displayZodSign(evt, 'trap');
 }, false);
+//
+//
+//event listeners help
 
+helpObj.addEventListener('click', function(evt) {
+	helpClick(evt, 'help');
+}, false);
+
+helpMenuObj.addEventListener('click', function(evt) {
+	closeHelp(evt, 'help-menu');
+}, false);
+
+
+//
+//
+//functions
+
+function helpClick(evt) {
+	helpMenuObj.classList.add('help-menu-active');
+	console.log('it werk');
+}
+
+function closeHelp(evt) {
+	helpMenuObj.classList.remove('help-menu-active');
+}
 
 
 function displayZodSign(evt, whichOne) {
@@ -159,72 +206,84 @@ function displayZodSign(evt, whichOne) {
 			//display big image for pis
 			//displayObj.src='img/ful_pisces.png';
 			imgArray[3].classList.add('active');
+			outputArea.innerHTML = descAloe;
 			break;
 
 		case 'bleeding':
 			//display big image for pis
 			//displayObj.src='img/ful_scorpio.png';
 			imgArray[8].classList.add('active');
+			outputArea.innerHTML = descBleeding;
 			break;
 
 		case 'bonsai':
 			//display big image for pis
 			//displayObj.src='img/ful_aquarius.png';
 			imgArray[1].classList.add('active');
+			outputArea.innerHTML = descBonsai;
 			break;
 
 		case 'cactus':
 			//display big image for pis
 			//displayObj.src='img/ful_pisces.png';
 			imgArray[5].classList.add('active');
+			outputArea.innerHTML = descCactus;
 			break;
 
 		case 'carnation':
 			//display big image for pis
 			//displayObj.src='img/ful_pisces.png';
 			imgArray[4].classList.add('active');
+			outputArea.innerHTML = descCarnation;
 			break;
 
 		case 'daffodil':
 			//display big image for pis
 			//displayObj.src='img/ful_pisces.png';
 			imgArray[6].classList.add('active');
+			outputArea.innerHTML = descDaffodil;
 			break;
 
 		case 'daisy':
 			//display big image for pis
 			//displayObj.src='img/ful_pisces.png';
 			imgArray[2].classList.add('active');
+			outputArea.innerHTML = descDaisy;
 			break;
 
 		case 'hanging':
 			//display big image for pis
 			//displayObj.src='img/ful_pisces.png';
-			imgArray[11].classList.add('active');
+			imgArray[10].classList.add('active');
+			outputArea.innerHTML = descHanging;
 			break;
 
 		case 'ivy':
 			//display big image for pis
 			//displayObj.src='img/ful_pisces.png';
-			imgArray[10].classList.add('active');
+			imgArray[11].classList.add('active');
+			outputArea.innerHTML = descIvy;
 			break;
 
 		case 'sun':
 			//display big image for pis
 			//displayObj.src='img/ful_pisces.png';
 			imgArray[9].classList.add('active');
+			outputArea.innerHTML = descSun;
 			break;
 
 		case 'tulip':
 			//display big image for pis
 			//displayObj.src='img/ful_pisces.png';
 			imgArray[0].classList.add('active');
+			outputArea.innerHTML = descTulip;
 			break;
 
 		case 'trap':
 			//display big image for pis
 			//displayObj.src='img/ful_pisces.png';
 			imgArray[7].classList.add('active');
+			outputArea.innerHTML = descVenus;
 			break;
 
 		default:
@@ -232,3 +291,6 @@ function displayZodSign(evt, whichOne) {
 			imgArray[0].classList.add('active');
 	}
 }
+
+
+
